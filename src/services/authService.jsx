d0d66afa963d134b3580b32948ce63b28b836c25
saloniwarang20 <saloniwarang20@gmail.com>
2026-06-04 16:14:48 +0530

    Configure production API URL

--- src/services/authService.jsx
+++ src/services/authService.jsx
@@ -3,10 +3,10 @@ import axios from "axios";
 const API_URL = `${import.meta.env.VITE_API_URL}`;
 
 export const signup = async (userData) => {
-    return axios.post(`${API_URL}/auth/signup`,userData);
+    return axios.post(`${API_URL}/api/auth/signup`,userData);
 }
 
 export const login = async (loginData) => {
-    return axios.post(`${API_URL}/auth/login`,loginData);
+    return axios.post(`${API_URL}/api/auth/login`,loginData);
 }
 
